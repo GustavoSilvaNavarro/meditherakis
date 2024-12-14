3af6ea749ab7f2a95f498bef845063d9c4af2014
Gustavo Silva <gustavo.silva.nav@gmail.com>
2024-12-14 18:17:31 -0300

    :sparkles: Making screen touchable  anbd scrawllable

--- src/screens/meditations/MeditationDisplays.tsx
+++ src/screens/meditations/MeditationDisplays.tsx
@@ -1,15 +1,22 @@
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
 
 export const MeditationDisplays = () => {
+  const handlePress = () => {
+    // Handle the press event here
+    console.log("Text pressed!");
+  };
+
   return (
     <ScrollView
       contentContainerStyle={styles.scrollViewContainer}
       style={styles.scrollView}
       keyboardShouldPersistTaps="handled">
-      <Text variant="displayLarge" style={styles.quote}>
+      <TouchableOpacity onPress={handlePress} activeOpacity={1}>
+      <Text variant="displayMedium" style={styles.quote}>
         "Time is your most valuable resource. And if you think about it, your only resource."
       </Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -31,3 +38,47 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 });
+
+
+
+// // import { ScrollView, StyleSheet } from 'react-native';
+// // import { Text } from 'react-native-paper';
+// // import { TouchableOpacity } from 'react-native';
+
+// export const MeditationDisplays = () => {
+//   const handlePress = () => {
+//     // Handle the press event here
+//     console.log("Text pressed!");
+//   };
+
+//   return (
+//     <ScrollView
+//       contentContainerStyle={styles.scrollViewContainer}
+//       style={styles.scrollView}
+//       keyboardShouldPersistTaps="handled">
+//       <TouchableOpacity onPress={handlePress}>
+//         <Text variant="displayLarge" style={styles.quote}>
+//           "Time is your most valuable resource. And if you think about it, your only resource."
+//         </Text>
+//       </TouchableOpacity>
+//     </ScrollView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollViewContainer: {
+//     flexGrow: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     padding: 20,
+//   },
+//   scrollView: {
+//     flex: 1,
+//     backgroundColor: 'purple',
+//   },
+//   quote: {
+//     textAlign: 'center',
+//     color: '#fff',
+//     paddingHorizontal: 20,
+//   },
+// });
